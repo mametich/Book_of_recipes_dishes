b13d55ed8c4e4ffc70d03bb76238a3e28ba3e27b
Rustem <mametich@rambler.ru>
2024-07-04 23:44:06 +0300

    RA-07-create_callBack

--- app/src/main/java/com/example/burgershop/CategoriesListAdapter.kt
+++ app/src/main/java/com/example/burgershop/CategoriesListAdapter.kt
@@ -9,12 +9,20 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.burgershop.databinding.ItemCategoryBinding
-import java.io.InputStream
 
 
 class CategoriesListAdapter(
     private val dataset: List<Category>
 ) : RecyclerView.Adapter<CategoriesListAdapter.CategoriesViewHolder>() {
+
+    interface OnItemClickListener {
+        fun onItemClick()
+    }
+    private var itemClickListener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        itemClickListener = listener
+    }
 
     class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val imageView: ImageView = view.findViewById(R.id.imageCategory)
@@ -30,6 +38,10 @@ class CategoriesListAdapter(
 
     override fun onBindViewHolder(holder: CategoriesViewHolder, position: Int) {
         val category = dataset[position]
+
+        holder.itemView.setOnClickListener {
+            itemClickListener?.onItemClick()
+        }
 
         holder.titleTextView.text = category.title
         holder.descriptionTextView.text = category.description
